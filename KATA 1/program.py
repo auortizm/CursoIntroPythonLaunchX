61a88d5970dd04adc539b13be766a009fe667a17
Arturo Ortiz Moncada <arturo.ortiz.m@outlook.com>
2022-02-09 21:10:06 -0600

    Se agregan katas 0-3 con ejemplos usados para practicar

---
+++ KATA 1/program.py
@@ -0,0 +1,21 @@
+sum = 1 + 2  # 3
+product = sum * 2
+print(product)
+
+planetas_en_el_sistema_solar = 8 # int, plutón era considerado un planeta pero ya es muy pequeño
+distancia_a_alfa_centauri = 4.367 # float, años luz
+puede_despegar = True
+transbordador_que_aterrizo_en_la_luna = "Apollo 11" #string
+
+distancia_a_alfa_centauri = 4.367 # Parece un decimal flotante
+
+print(type(distancia_a_alfa_centauri))
+
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(int(first_number) + int(second_number))
