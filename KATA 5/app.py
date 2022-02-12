7d54b89a4f8dbaf4d82f549b486d24d3dcd9df48
Arturo Ortiz Moncada <arturo.ortiz.m@outlook.com>
2022-02-12 01:57:58 -0600

    add: Se agrega kata 5 con ejemplos de funciones en app.py

---
+++ KATA 5/app.py
@@ -0,0 +1,46 @@
+# Archivo de ejemplo para practicar las funciones de la documentación
+
+answer = 30 + 12
+print(answer)
+
+difference = 30 - 12
+print(difference)
+
+product = 30 * 12
+print(product)
+
+quotient = 30 / 12
+print(quotient)
+
+# Obtener minutos y segundos
+seconds = 1042
+display_minutes = 1042 // 60
+print(display_minutes)
+
+seconds = 1042
+display_minutes = 1042 // 60
+display_seconds = 1042 % 60
+
+print('Minutos: ' + str(display_minutes))
+print('Segundos: ' + str(display_seconds))
+
+
+result_1 = 1032 + 26 * 2
+result_2 = 1032 + (26 * 2)
+
+print(result_1)
+print(result_2)
+
+print(abs(39 - 16))
+print(abs(16 - 39))
+
+print(round(14.5))
+
+from math import ceil, floor
+
+round_up = ceil(12.5)
+print(round_up)
+
+round_down = floor(12.5)
+print(round_down)
+
