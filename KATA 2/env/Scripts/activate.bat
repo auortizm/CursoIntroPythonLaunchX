ea33aec3fa7e1bea3fae0610b06d9cfd0c53992f
Arturo Ortiz Moncada <arturo.ortiz.m@outlook.com>
2022-02-16 01:29:22 -0600

    edit: se agregó la captura de pantalla de la ejecución correcta de comandos

--- KATA 2/env/Scripts/activate.bat
+++ KATA 2/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Documentos\Cursos\KATA 1\env
+set VIRTUAL_ENV=D:\Documentos\Cursos\KATA 2\env
 
 if not defined PROMPT set PROMPT=$P$G
 
